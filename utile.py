0233521639cf1cae4772e134b48f64116f3bb06e
FatiH2O <101948894+FatiH2O@users.noreply.github.com>
2024-04-17 16:51:42 +0200

    Update utile.py
    correction de somme distance, tout fonctionne sauf selectionnerparent pour l'instant

--- utile.py
+++ utile.py
@@ -1,3 +1,5 @@
+
+
 from math import sqrt  # Importe la fonction sqrt pour calculer la racine carrée
 from random import uniform  # Importe la fonction uniform pour générer des nombres aléatoires dans une plage donnée
 
@@ -5,14 +7,21 @@ def Distance(a, b):
     # Calcule la distance entre deux points a et b dans un espace 2D
     return sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
 
-def sommeDistances(points):
-    # Calcule la somme des distances entre les points dans une liste de points
+def sommeDistances(individu, coordonnee):
+    # Calcule la somme des distances entre les villes dans une liste d'individus et les coordonnées des villes (dictionnaire)
     s = 0
-    for i in range(len(points)):
-        # Calcule la distance entre chaque point et le point suivant dans la liste
-        dist = Distance(points[i], points[(i + 1) % len(points)])
+    for i in range(len(individu)):
+        # Calcule la distance entre chaque ville et la ville suivante dans l'individu
+        ville_actuelle = individu[i]
+        # Utilisation de l'opérateur modulo pour gérer le cas où nous sommes à la dernière ville
+        ville_suivante = individu[(i + 1) % len(individu)]
+        dist = Distance(coordonnee[ville_actuelle], coordonnee[ville_suivante])
         s += dist  # Ajoute la distance à la somme totale
+        print("somme distance est:", s)
+    print("he",s)
     return s
+
+
 
 def selectionnerParent(liste, probabilites):
     # Sélectionne un parent de la liste en fonction des probabilités données
